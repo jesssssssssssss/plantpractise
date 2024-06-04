45acf8cf682f8d8427f89b896edb4cfe17885816
jesssssssssssss <jessica_sharma11@hotmail.com>
2024-06-04 21:49:33 +1200

    added additional validation requirements

--- 1Plant Website/Plant Website/Website/views.py
+++ 1Plant Website/Plant Website/Website/views.py
@@ -73,14 +73,27 @@ def editAccountDetails():
         paymentCardCvc = request.form['paymentCardCvc']
         paymentCardExp = request.form['paymentCardExp']
 
-        # validate here
+        # Validation happens here. Please Note: firstName attribute is being tested with both client-side validation 
+        #  (by using the "required" keyword in the html file), and with server-side validation, here. This is because
+        #  using just client-side validation in any context isn't completely secure and can be bypassed by users that
+        #  have developer tools and knowledge, and adding server-side validation provides extra security. Therefore,
+        #  the use of both these methods is not in conflict with the DRY principle. 
+        
         if not firstName:
             flash("First name is required", category="error")
+
+        elif not email:
+            flash("Email is required", category="error")
 
         else:
             if len(mobileNo) < 5:
                 flash("Mobile num must be at least 5 digits", category="error")
                 return redirect(url_for('views.editAccountDetails'))
+
+            elif len(firstName) < 2:
+                flash("first nam must be at least 2 characters", category="error")
+                return redirect(url_for('views.editAccountDetails'))
+            
      
             else:
 
@@ -105,32 +118,4 @@ def editAccountDetails():
     else:
         return render_template("editAccountDetails.html", user=current_user) 
 
-@views.route('/completeEdit', methods=['POST', 'GET'])
-def completeEdit():
-    if request.method == 'GET':
-        try:
-            UsrID = request.args.get('UsrID')
-            firstName = request.args.get('firstName')
-            lastName = request.args.get('lastName')
-            email = request.args.get('email')
-            mobileNo = request.args.get('mobileNo')
-            addressHouseNo = request.args.get('addressHouseNo')
-            addressStreetName = request.args.get('addressStreetName')
-            addressSuburb = request.args.get('addressSuburb')
-            addressCity = request.args.get('addressCity')
-            addressPostCode = request.args.get('addressPostCode')
-
-            with sql.connect("database.db") as con:
-                cur=con.cursor()
-                cur.execute("UPDATE AccountDetails SET firstName=?, lastName=?, email=?, mobileNo=?, addressHouseNo=?, addressStreetName=?, addressSuburb=?, addressCity=?, addressPostCode=? WHERE UsrID=?", (firstName, lastName, email, mobileNo, addressHouseNo, addressStreetName, addressSuburb, addressCity, addressPostCode, UsrID))
-                msg = "Data updated success"
-                con.commit()
-        
-        except: 
-            msg = "error in update"
-            con.rollback()
-            
-        finally:
-            return render_template("completeEdit.html", msg=msg)
-        
 
